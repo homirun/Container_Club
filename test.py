499886a0f5445c38d5ed0fe77a51e196a58af221
homirun <hatunemiku.moco@gmail.com>
2018-06-12 17:37:13 +0900

    add Comment

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 from curses import wrapper
 import curses
 from time import sleep
+
 def main(stdscr):
+    # clearコマンド
     stdscr.clear()
+
     for i in range(30):
         stdscr.addstr(str(i))
         stdscr.addstr(i, i, "hoge",curses.A_REVERSE)
         stdscr.refresh()
+        # スリープ挟まんと死ぬ
         sleep(0.1)
         stdscr.clear()
 
     stdscr.refresh()
+
+    # 多分キー取ってる
     stdscr.getkey()
 
 if __name__ == '__main__':
